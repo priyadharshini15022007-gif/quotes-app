6d7be07beb54f93fe25e63ce441e4ebf839f0209
priyadharshini15022007-gif <priyadharshini15022007@gmail.com>
2026-03-30 22:01:10 +0530

    Update auth.js

--- backend/auth.js
+++ backend/auth.js
@@ -1,49 +1,32 @@
 const express = require("express");
-const session = require("express-session");
+const router = express.Router();
+
 const passport = require("passport");
-const GitHubStrategy = require("passport-github2").Strategy;
-require("dotenv").config();
 
-const app = express();
-app.use(express.json());
-app.use(require("cors")());
+// Normal login
+router.post("/login", (req, res) => {
+    const { user, pass } = req.body;
 
-app.use(session({ secret: "secret", resave: false, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
+    if (user === "priya" && pass === "1234") {
+        res.json({ success: true });
+    } else {
+        res.json({ success: false });
+    }
+});
 
-// Import routes
-const authRoutes = require("./auth");
-const quoteRoutes = require("./quotes");
-
-app.use("/", authRoutes);
-app.use("/", quoteRoutes);
-
-// GitHub Auth
-passport.use(new GitHubStrategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://127.0.0.1:5000/auth/github/callback"
-},
-(accessToken, refreshToken, profile, done) => {
-    return done(null, profile);
-}));
-
-passport.serializeUser((user, done) => done(null, user));
-passport.deserializeUser((obj, done) => done(null, obj));
-
-// GitHub routes
-app.get("/auth/github",
+// GitHub login route
+router.get("/github",
     passport.authenticate("github", { scope: ["user:email"] })
 );
 
-app.get("/auth/github/callback",
+// GitHub callback
+router.get("/github/callback",
     passport.authenticate("github", {
-        failureRedirect: "/"
+        failureRedirect: "/login.html"
     }),
     (req, res) => {
-        res.redirect("http://127.0.0.1:5500/frontend/quotes.html");
+        res.redirect("https://quotes-app-alpha-lemon.vercel.app/quotes.html");
     }
 );
 
-app.listen(5000, () => console.log("Server running"));
+module.exports = router;
